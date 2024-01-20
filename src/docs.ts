b9f6939f92cee0a24013a8b195260ca13f6b31eb
steve-scorn <sskillcorn@gmail.com>
2024-01-20 17:13:02 +1300

    Build script

--- src/docs.ts
+++ src/docs.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 import mammoth from "mammoth";
-import Turndown from "turndown";
-import * as plugin from "turndown-plugin-gfm";
+import { NodeHtmlMarkdown, NodeHtmlMarkdownOptions } from 'node-html-markdown';
 import { JSDOM } from "jsdom";
 
+const nhm = new NodeHtmlMarkdown();
 const DOC_URL = "https://docs.google.com/document/export?format=docx&id=";
 
 async function exportDoc(id: string, path: string) {
@@ -21,50 +21,15 @@ async function exportDoc(id: string, path: string) {
     // Convert to html
     const conversion = await mammoth.convertToHtml({ buffer: buffer });
     // Remove the non printing characters that docs code block adds
-    let html = conversion.value.replace("", "").replace("", "");
-    console.log(html);
-    // Adjust tables
-    html = table(html);
+    const html = conversion.value.replace("", "").replace("", "");
+    //console.log(html);
     const fm = frontmatter(html);
     // Convert the html to markdown
-    const turndown = new Turndown({ headingStyle: "atx" }).use(plugin.gfm).addRule("break", {
-        filter: ["br"],
-        replacement: () => "<br />",
-    });
-    const markdown = fm.frontmatter + "\n" + turndown.turndown(fm.body);
+    const markdown = nhm.translate(fm.body);
     // Write the markdown to a file
     const fullPath = path + "/" + filename.replace(".docx", ".md");
-    fs.writeFileSync(fullPath, markdown);
+    fs.writeFileSync(fullPath, fm.frontmatter + markdown);
     console.log("Markdown written to", fullPath);
-}
-
-// Make adjustments to a table's html, making it frendly for turndown
-function table(html: string) {
-    const dom = new JSDOM(html);
-    const tables = dom.window.document.body.querySelectorAll("table");
-    for (const table of tables) {
-        const rewritten: string[] = [];
-        const rows = table.querySelectorAll("tr");
-        for (let i = 0; i < rows.length; i++) {
-            const row = rows[i];
-            rewritten.push("<tr>");
-            for (const cell of row.children) {
-                // Make the first row a header - turndown requires this
-                rewritten.push(i ? "<td>" : "<th>");
-                // Remove paragraphs from the cell content
-                const content: string[] = [];
-                for (const p of cell.children) {
-                    content.push(p.innerHTML);
-                }
-                // Break paragraphs with a <br>
-                rewritten.push(content.join("<br>"));
-                rewritten.push(i ? "</td>" : "</th>");
-            }
-            rewritten.push("</tr>");
-        }
-        table.innerHTML = rewritten.join("");
-    }
-    return dom.window.document.body.innerHTML;
 }
 
 // Detect and handle front-matter before converting to markdown
@@ -82,7 +47,7 @@ function frontmatter(html: string) {
         text.push("---");
         return {
             body: html.replace(finder, ""),
-            frontmatter: text.join("\n"),
+            frontmatter: text.join("\n") + "\n",
         };
     }
     return {
